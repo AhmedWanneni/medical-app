13867585a808dae0e837f96c837956573f0058e5
Ahmed Wanneni <ahmed.wanneni@gmail.com>
2024-04-29 08:05:36 +0100

    fix: fix link

--- api/server.js
+++ api/server.js
@@ -20,13 +20,13 @@ app.use("/api/doctor", doctorRoute);
 
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use("/", express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+//   });
+// }
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
